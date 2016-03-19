7369239ffc233d14940d2c1629a1de372531caf5
zdajani <dajani.zeina@gmail.com>
2016-03-19 19:54:15 +0100

    added required gems for testing and starting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  config.action_mailer.default_url_options = { host: 'ec2-54-83-22-48.compute-1.amazonaws.com', port: 5432 }
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
