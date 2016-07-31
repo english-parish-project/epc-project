e65034a9847a8defcb0b22ffb491bb1d1bc47fbb
zdajani <dajani.zeina@gmail.com>
2016-07-31 13:33:21 +0200

    add: lady chapel to church show

--- spec/factories/lady_chapels.rb
+++ spec/factories/lady_chapels.rb
@@ -1,5 +1,10 @@
 FactoryGirl.define do
   factory :lady_chapel do
-    
+    date "700-725"
+    date_secured true
+    date_evidence "lady chapel date information"
+    styles ["Anglo-Saxon"]
+    location "lady chapel location"
+    notes "lady chapel notes"
   end
 end
